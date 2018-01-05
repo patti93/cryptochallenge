4cc1c96ae62a990d3317c3bb18f95d7393c44b91
henshauk <Hauke@Hauke>
2018-01-05 16:50:44 +0100

    Kommentare eingefügt

--- CryptoChallenge/src/src/Solve.java
+++ CryptoChallenge/src/src/Solve.java
@@ -6,68 +6,78 @@ import java.util.Arrays;
 public class Solve {
 
 	public static ArrayList<int[]> rmNull(int[][] m, int length) {
+		// länge der Zeilen
 		length = length * length;
 		ArrayList<int[]> o = new ArrayList<int[]>();
 		ArrayList<int[]> l = new ArrayList<int[]>(Arrays.asList(m));
-
 		boolean oneFound = false;
+		
+		// vergleichs-Array für 0 Zeilen
 		int[] test = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
+		//schleife für Zeilen
 		for (int i = 0; i < l.size(); i++) {
+			//Schleife für Spalten
 			for (int j = 0; j < l.get(i).length; j++) {
-				if (l.get(i)[j] == 1) {
+				if (l.get(i)[j] == 1) { //Falls 1 gefunden in Zeile i
 					oneFound = true;
 				}
 			}
-			if (oneFound == false) {
+			if (oneFound == false) { //Zeile entfernen, falls keine 1 gefunden wurde
 				l.remove(i);
 			}
-			oneFound = false;
+			oneFound = false; // rücksetzten des Wertes für nächste zeile
 		}
-
+		// verschieben alle beschriebenen Zeilen in neue Matrix
 		for (int i = 0; i < l.size(); i++) {
 			if (Arrays.equals(l.get(i), test)) {
 			} else {
 				o.add(i, l.get(i));
 			}
 		}
-
+		
+		// Ausgabe der Matrix
 		for (int i = 0; i < o.size(); i++) {
 			for (int j = 0; j < length; j++) {
 				System.out.print(o.get(i)[j] + " ");
 			}
 			System.out.println();
 		}
-		return o;
+		return o; //Rückgabe der Matrix
 	}
 
 	public static ArrayList<int[]> aufrollen(int[][] m, int[][] lsg) {
 		int[] temp = new int[m[0].length];
-		int rndCount = 0, countOne = 0;
+		int rndCount = 0, countOne = 0; // Zählen der bereits festgestellten einsen und der noch unbestätigketen in einer Zeile
+		
+		// Hilfs-listen/Matritzen für die Rekursion
 		ArrayList<int[]> templ2 = new ArrayList<int[]>();
 		ArrayList<int[]> templ1 = new ArrayList<int[]>();
 		int[][] templsg2 = new int[m.length][m[0].length];
 		int[][] tempm2 = new int[m.length - 1][m[0].length];
 		int[][] templsg1 = new int[m.length][m[0].length];
 		int[][] tempm1 = new int[m.length - 1][m[0].length];
+		// Alle Zeilen durchgehen
 		for (int i = m.length - 1; i >= 0; i--) {
-
+			// Alle Spalten einer Zeile durchgehen und "unbekannte" einsem notieren in temp array
 			for (int j = m[0].length - 1; j >= 0; j--) {
 				if (m[i][j] == 1 && lsg[0][j] == lsg[1][0]) {
 					rndCount++;
 					temp[j] = 1;
 				}
 			}
+			// 
 			for (int k = 0; k < m[0].length; k++) {
+				// Zählen Welche einsen bekannt sind
 				if (lsg[0][k] == 1 && temp[k] == 1) {
 					countOne++;
 				}
+				// Zählen Welche einen keine aussage treffen
 				if (lsg[0][k] == 0 && temp[k] == 1) {
 					rndCount--;
 				}
 			}
+			// Prüfen ob sich lösungsmöglichkeit auftrennt
 			if ((rndCount - countOne) % 2 == 0) {
-
 				tempm1 = m;
 				for (int l = m[0].length - 1; l >= 0; l--) {
 					if (temp[l] == 1) {
@@ -75,8 +85,9 @@ public class Solve {
 						templsg1[0][l] = 0;
 					}
 				}
-
+				//Rekursiv neuer Aufruf für neue Lösungsmöglichekit (mit verkleinerter matrix)
 				templ1 = Solve.aufrollen(tempm1, templsg1);
+				// Prüfen ob sich lösungsmöglichkeit auftrennt
 			} else if ((rndCount) % 2 == 0 && rndCount != 1) {
 
 				boolean second = false;
@@ -91,38 +102,38 @@ public class Solve {
 						templsg2[0][l] = 1;
 					}
 				}
-
+				//Rekursiv neuer Aufruf für neue Lösungsmöglichekit (mit verkleinerter matrix)
 				templ2 = Solve.aufrollen(tempm2, templsg2);
 
 			}
 		}
-
+		// Neue Arraylist für endgültige Lösung
 		ArrayList<int[]> loesung = new ArrayList<int[]>(Arrays.asList(lsg));
-
+		//Errechnete Lösungend der rekursiv aufrufe in die Lösung aufnehmen
 		if (!templ1.isEmpty()) {
 			loesung.addAll(templ1);
 		}
 		if (!templ2.isEmpty()) {
 			loesung.addAll(templ2);
 		}
-
+		//rückgabe der Lösung
 		return loesung;
 
 	}
 
 	public static ArrayList<int[]> einsetzen(ArrayList<int[]> m, int[] c) {
-
+		//Matrix in zweidimensionales Array aufnehmen
 		int[][] l = new int[m.size()][m.get(0).length];
 		l = m.toArray(l);
-
-		for (int i = 0; i < (l.length / 2); i++) {
-			System.out.println(Arrays.toString(l[i]));
-		}
+		
+		
 		int r = 0;
+		// alle Zeilen durchiterieren
 		for (int i = 0; i < m.size(); i++) {
+			// alle spalten durchiterieren
 			for (int j = 0; j < m.get(0).length; j++) {
-				r = Math.round(j / c.length);
-				System.out.println("i= " + i + "    j = " + j + "     r= " + r);
+				r = Math.round(j / c.length); // Richtiges Array des Chiffrats errechnen
+				//Vergleichen, ob Chiffrat und x = 1 sind, falls ja 1 einfügen ansonsten 0
 				if (m.get(i)[j] == 1 && c[r] == 1) {
 					l[i][j] = 1;
 				} else {
@@ -130,13 +141,13 @@ public class Solve {
 				}
 			}
 		}
+		// nullen entfernen
 		rmNull(l, c.length);
-
-		for (int i = 0; i < l.length; i++) {
-			System.out.println(Arrays.toString(l[i]));
-		}
-
-		return null;
+		
+		ArrayList<int[]> loesung = new ArrayList<int[]>(Arrays.asList(l));
+		
+		//Rückgabe der neuen Matrix
+		return loesung;
 	}
 
 }
